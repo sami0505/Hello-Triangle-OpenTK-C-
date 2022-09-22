fc33f2392b9ea22ca8a0f07c9c9b96c3a4a32a49
sami0505 <sami0505@abv.bg>
2022-09-22 08:36:31 +0100

    Add project files.

---
+++ triangleOpenTK/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace triangleOpenTK
+{
+    public class Shader
+    {
+        int Handle;
+        private bool disposedValue = false;
+        
+        // Constructor
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            // Shaders source code is taken into mem
+
+            string VertexShaderSource = File.ReadAllText(vertexPath);
+            string FragmentShaderSource = File.ReadAllText(fragmentPath);
+
+            // Shader objects are created and bound to source
+            var VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            var FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+
+            // Compiling the Vertex Shader
+            int success;
+
+            GL.CompileShader(VertexShader);
+
+            GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(VertexShader);
+                Console.WriteLine(infoLog);
+            }
+
+            // Compiling the Fragment Shader
+
+            GL.CompileShader(FragmentShader);
+
+            GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(FragmentShader);
+                Console.WriteLine(infoLog);
+            }
+
+            // Linking Shaders to Program
+            Handle = GL.CreateProgram();
+
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            GL.LinkProgram(Handle);
+
+            GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetProgramInfoLog(Handle);
+                Console.WriteLine(infoLog);
+            }
+
+            // After Linking, shaders must be detached and deleted.
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(VertexShader);
+            GL.DeleteShader(FragmentShader);
+        }
+        
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        // Disposer
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+                disposedValue = true;
+            }
+        }
+
+        // Destructor (needed in case of crash (most likely (source: The Elephant Guy) ))
+        ~Shader()
+        {
+            GL.DeleteProgram(Handle);
+        }
+
+        // In the case there is no specified bool disposing, default is true
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+    public class Game : GameWindow
+    {
+        Shader shader;
+        int VertexBufferObject;
+        int VertexArrayObject;
+
+        // Constructor. Defines Game as a GameWindow child, with height width and title
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) {}
+
+        // We override the OnUpdateFrame to make keyboard input be detected
+        protected override void OnUpdateFrame(FrameEventArgs args) {
+            base.OnUpdateFrame(args);
+
+            KeyboardState input = KeyboardState;
+
+            if (input.IsKeyDown( Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        //
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            // Specifies the "Background color", which overwrites the screen on GL.Clear(), in this case, a dark blue-ish green.
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            shader = new Shader("shader.vert", "shader.frag");
+
+            VertexArrayObject = GL.GenVertexArray();
+
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+        }
+
+        // When closed, the window disposes of the shaders.
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            shader.Dispose();
+        }
+
+        //
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        float[] vertices =
+        {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+        };
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] Args)
+        {
+            Game game = new Game(800, 600, "Grand Theft Auto: San Andreas");
+            game.Run();
+        }
+    }
+}
